c53e934a0a3b554fb6d6347823d5e85e6bcd86e9
Rajalakshmi L <rajyashreelrl96@gmail.com>
2025-12-04 11:33:06 +0530

    Exercise 2

--- PycharmProjects/test1/main.py
+++ PycharmProjects/test1/main.py
@@ -1,32 +1,158 @@
-import random
-
-# Number Guessing game(1 to 50)
-print("Number Guessing game")
-num = random.randint(1, 50)
-while True:
-    user_input = int(input("Guess a number between 1-50: "))
-    if user_input < num:
-        print("Your guess is too low")
-    elif user_input > num:
-        print("Your guess is too high")
+#1.to find two list contains even number list and odd number list
+list = [10,501,22,37,100,999,87,351] #list of given numbers
+#creating empty list to store even numbers and odd numbers
+even_numbers = []
+odd_numbers = []
+#loop through each numbers in the list
+for numbers in list:
+    #check if the number is even
+    if numbers %2 == 0:
+        even_numbers.append(numbers)
     else:
-        print("your guess is Correct!")
-        break
+        odd_numbers.append(numbers)
+        #print the result
+print("Even numbers:", even_numbers)
+print("odd numbers:", odd_numbers)
 
-# Scramble list of words
-words = ["python", "javascript", "java", "automation", "pytest", "guvi", "selenium"]
+#2.To count prime numbers and print list of prime numbers
+integers = [10,501,22,37,100,999,87,351]
+def prime(num):
+    if num<=1:
+        return False
+    else:
+        for i in range(2, int(num*0.5)+1): #iterating to find a prime number
+            if num % i == 0:
+                return False
+        return True
+prime_numbers= []
+for n in integers:
+    if prime(n):
+        prime_numbers.append(n)
+        print("Prime numbers:", prime_numbers)
+        print("count of prime numbers:", len(prime_numbers))
 
-print("Unscramble the programming language names!")
-for word in words:
-    word_list = list(word)
-    random.shuffle(word_list)
-    scrambled_word = ''.join(word_list)
+#3.TO finding happy numbers
+def hppy_numbers(n): #functionn ti find a hppy numbers
+    seen = set()
+    while n!=1 and n not in seen:
+        seen.add(n)
+        sum_square = sum(int(digit)**2 for digit in str(n))
+        n = sum_square
+    return n == 1
+numbers = [10, 501, 22, 37, 100, 999, 87, 351]
+happy_count=0 #To count a happy numbers in a given list
+happy_list=[]
+for num in numbers:
+        if hppy_numbers(num):
+            happy_count += 1
+            happy_list.append(num)
+            print("Happy numbers are ", happy_list)
+            print("the count of happy numbers:", happy_count)
 
-    while True:
-        print("Scramble word : " + scrambled_word)
-        guess = input("Your guess: ").lower()
-        if guess == word:
-            print("Correct!")
-            break
+#4.To find the sum of the first and last digit of an integer
+
+number = int(input("Enter the number: "))
+last_digit = number%10
+first_digit = number
+while first_digit>10:
+    first_digit //= 10
+result = first_digit + last_digit
+print("sum of the first and last digit is ", result)
+
+#5.Ways to make 10rs.coin using rs.1, rs.2, rs.5 and rs.10 coins
+print("the way of to make 10rs coin")
+count = 0
+for c1 in range(2):
+    for c2 in range(3):
+        for c5 in range(6):
+            for c10 in range(11):
+                total = c1*10 + c2*5 + c5*2 + c10*1
+                if total == 10:
+                    count += 1
+                    print("1rs:{c1}, 2rs:{c2}, 3rs:{c5}, 4rs:{c10}".format(**locals()))
+from distutils.spawn import find_executable
+
+#6. Find the duplicates in given list
+number1 = [44, 55,44, 20, 22, 33]
+number2 = [4, 5,4, 20, 2, 3,5]
+number3 = [20,30,40,20,30]
+duplicates = set(number1) & set(number2) & set(number3) #using intersection for across all the list of numbers
+print("duplicates in the all 3 lists:", duplicates)
+
+def find_duplicates(numbers):
+    seen = set()
+    duplicates = set()
+    for num in numbers:
+        if num in seen:
+            duplicates.add(num)
         else:
-            continue
+            seen.add(num)
+    print(duplicates)
+
+find_duplicates(number1)
+find_duplicates(number2)
+find_duplicates(number3)
+
+
+
+#7.find the first non-repeating elements
+numbers = [4, 5, 1, 2, 0, 4, 5, 2]
+#to find the first non-repeating element
+first_non_repeat = None
+for num in numbers:
+    if numbers.count(num) == 1:
+        first_non_repeat = num
+        break
+print("First non-repeating element:", first_non_repeat)
+
+#8.To find the minimum element in a rated and sorted list
+nums = [23, 45, 56, 11, 99, 77]
+print("Minimum element in a given list", min(nums)) #using min function to find the minimum element
+
+#9.To find a triplet in a given list whose sum is equal to 59
+nums = [10, 20, 30, 9]
+value = 59
+found = False
+#loop starts through all triplets
+for i in range(len(nums)):
+    for j in range(i+1, len(nums)):
+        for k in range(j+1, len(nums)):
+            if nums[i]+nums[j]+nums[k] == value:
+                print("triplet of the list is" ,nums[i], nums[j], nums[k])
+                found = True
+if not found:
+    print("the list isn't found")
+
+#10.In a given list to find a sub-list whose sum is zero
+def sub_list(nums):
+    prefix_sum = {}
+    current_sum = 0
+
+    for i in range(len(arr)):
+        current_sum += arr[i]
+
+        #sub-list from index 0 to i has sum = 0
+        if current_sum == 0:
+            return (0, i)
+
+        #if current_sum already occurred, sub-list is between previous index+1 to i
+        if current_sum in prefix_sum:
+            return (prefix_sum[current_sum] + 1, i)
+
+        prefix_sum[current_sum] = i
+
+    return None
+
+arr = [4, 2, -3, 1, 6]
+result = sub_list(arr)
+
+if result:
+    start, end = result
+    print("Zero-sum sub-list found:", arr[start:end + 1])
+else:
+    print("No zero-sum sub-list found.")
+
+
+
+
+
